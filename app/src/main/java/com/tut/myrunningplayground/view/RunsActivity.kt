4f68bbf30fba6308d6f7e389d3c7967f88be4bcb
Shay Buzz <shaybuzz@gmail.com>
2020-07-08 00:21:43 +0300

    bottomnav

--- app/src/main/java/com/tut/myrunningplayground/view/RunsActivity.kt
+++ app/src/main/java/com/tut/myrunningplayground/view/RunsActivity.kt
@@ -3,22 +3,21 @@ package com.tut.myrunningplayground.view
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.tut.myrunningplayground.R
 import com.tut.myrunningplayground.db.entity.Run
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
-@AndroidEntryPoint
+
 class RunsActivity : AppCompatActivity() {
-
-    private val runsViewModel: RunsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        runsViewModel.add(Run(null, 4, 4, 4, 5, 6f))
-//        runsViewModel.add(Run(null, 74, 64, 54, 55, 6f))
-//
-//        runsViewModel.fetchData()
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(findNavController(R.id.navHostFragment))
 
     }
 }
