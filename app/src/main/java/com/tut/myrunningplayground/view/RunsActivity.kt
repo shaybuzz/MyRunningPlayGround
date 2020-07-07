da0213a5c4361d5d6f26e6607627b6b6a20b81aa
Shay Buzz <shaybuzz@gmail.com>
2020-07-08 00:42:39 +0300

    style

--- app/src/main/java/com/tut/myrunningplayground/view/RunsActivity.kt
+++ app/src/main/java/com/tut/myrunningplayground/view/RunsActivity.kt
@@ -1,23 +1,32 @@
 package com.tut.myrunningplayground.view
 
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.tut.myrunningplayground.R
-import com.tut.myrunningplayground.db.entity.Run
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
-
+@AndroidEntryPoint
 class RunsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        bottomNavigationView.setupWithNavController(findNavController(R.id.navHostFragment))
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.statisticsFragment, R.id.runsFragment -> bottomNavigationView.visibility =
+                        View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
 
     }
 }
--- app/src/main/java/com/tut/myrunningplayground/view/RunsFragment.kt
+++ app/src/main/java/com/tut/myrunningplayground/view/RunsFragment.kt
@@ -1,12 +1,22 @@
 package com.tut.myrunningplayground.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.tut.myrunningplayground.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunsFragment:Fragment(R.layout.fragment_run) {
 
     val runsViewModel:RunsViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runsFragment_to_trackingFragment)
+        }
+    }
 }
